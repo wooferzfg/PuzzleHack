a1755c9fe6faf19471aa36d987f5290c78656704
Sergey Papushin <spapushin@gmail.com>
2016-03-13 15:55:44 -0500

    Add warp after Intro

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -8,10 +8,10 @@ use libtww::prelude::*;
 use libtww::system;
 use libtww::Link;
 use libtww::game::Console;
-use libtww::link::song;
 use libtww::game::flag;
-use libtww::game::controller;
+use libtww::warping::Entrance;
 use libtww::warping::Warp;
+use libtww::warping::stage;
 
 #[no_mangle]
 #[inline(never)]
@@ -29,23 +29,25 @@ pub extern "C" fn game_loop() {
     let console = Console::get();
     let mut lines = &mut console.lines;
 
-    let _ = write!(lines[0].begin(), "Stage:          {}", Link::stage());
-    let _ = write!(lines[1].begin(), "Room:           {}", Link::room());
+    let exit = Warp::last_exit();
+
+    let _ = write!(lines[0].begin(), "Stage:          {}", Entrance::last_entrance().stage_name());
+    let _ = write!(lines[1].begin(), "Room:           {}", Entrance::last_entrance().room);
     let _ = write!(lines[2].begin(), "Rupees:         {}", link.rupees);
     let _ = write!(lines[3].begin(), "Sword ID:       {:02X}", link.sword_id);
     let _ = write!(lines[4].begin(), "Shield ID:      {:02X}", link.shield_id);
     let _ = write!(lines[5].begin(), "Max Magic:      {}", link.max_magic);
     let _ = write!(lines[6].begin(), "Magic:          {}", link.magic);
-}
-
-#[no_mangle]
-#[inline(never)]
-pub extern "C" fn init_save_file() {
+    
+    if exit.entrance.stage_name() == stage::sea::SEA && Link::stage() == stage::other::NAME_SELECT && !flag::HAS_SEEN_INTRO.is_active()
+    {
+        let warp = Warp::new(stage::earth_temple::TEMPLE, 1, 1, exit.layer_override, exit.fadeout, exit.enabled);
+        warp.execute();
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn start() {
     game_loop();
     init();
-    init_save_file();
 }
--- src/src/patch.asm
+++ src/src/patch.asm
@@ -6,6 +6,3 @@ bl init
 
 0x8000645C:
 bl game_loop
-
-0x8005b47c:
-b init_save_file
