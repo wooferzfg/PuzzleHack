5df117a81eb976976e39f4afd87eb92c8ae83ea4
Sergey Papushin <spapushin@gmail.com>
2016-03-13 23:06:04 -0500

    Add first three puzzles

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -6,8 +6,12 @@ extern crate libtww;
 
 use libtww::prelude::*;
 use libtww::system;
+use libtww::link;
 use libtww::Link;
+use libtww::link::inventory::Inventory;
+use libtww::link::item;
 use libtww::game::Console;
+use libtww::game::controller;
 use libtww::game::flag;
 use libtww::warping::Entrance;
 use libtww::warping::Warp;
@@ -27,22 +31,64 @@ pub extern "C" fn init() {
 pub extern "C" fn game_loop() {
     let link = Link::get();
     let console = Console::get();
+    let inventory = Inventory::get();
     let mut lines = &mut console.lines;
 
     let exit = Warp::last_exit();
-
-    let _ = write!(lines[0].begin(), "Stage:          {}", Entrance::last_entrance().stage_name());
-    let _ = write!(lines[1].begin(), "Room:           {}", Entrance::last_entrance().room);
-    let _ = write!(lines[2].begin(), "Rupees:         {}", link.rupees);
-    let _ = write!(lines[3].begin(), "Sword ID:       {:02X}", link.sword_id);
-    let _ = write!(lines[4].begin(), "Shield ID:      {:02X}", link.shield_id);
-    let _ = write!(lines[5].begin(), "Max Magic:      {}", link.max_magic);
-    let _ = write!(lines[6].begin(), "Magic:          {}", link.magic);
     
     if exit.entrance.stage_name() == stage::sea::SEA && Link::stage() == stage::other::NAME_SELECT && !flag::HAS_SEEN_INTRO.is_active()
     {
+        system::memory::write(0x803B81BC, 0x01000000); //hero's sword
         let warp = Warp::new(stage::earth_temple::TEMPLE, 1, 1, exit.layer_override, exit.fadeout, exit.enabled);
         warp.execute();
+    }
+    if Link::stage() == stage::earth_temple::TEMPLE && exit.entrance.room == Entrance::last_entrance().room
+    {
+        let _ = write!(lines[0].begin(), "Puzzle 1: Backwards is Forwards");
+
+        if Link::room() == 0 && exit.entrance.room == Entrance::last_entrance().room
+        {
+            system::memory::write(0x803B81BC, 0x00000000); //hero's sword off
+            let warp = Warp::new(stage::earth_temple::TEMPLE, 1, 1, exit.layer_override, exit.fadeout, true);
+            warp.execute();
+        }
+        else if Link::room() == 1
+        {
+            let warp = Warp::new(stage::earth_temple::TEMPLE, exit.entrance.entrance, exit.entrance.room, exit.layer_override, exit.fadeout, false);
+            warp.execute();
+        }
+    }
+
+    if Link::stage() == stage::forsaken_fortress::FF1_INTERIOR
+    {
+
+        if !flag::GRABBED_FIRST_ROPE_IN_FF1.is_active()
+        {
+            let _ = write!(lines[0].begin(), "Puzzle 2: On the Ropes Without That Button");
+            Link::set_collision(link::CollisionType::DoorCancel);
+        }
+        else
+        {
+            let _ = write!(lines[0].begin(), "Puzzle 3: No Pain, No Gain");
+            if link.heart_quarters < 12
+            {
+                Link::set_collision(link::CollisionType::ChestStorage);
+            }
+            else
+            {
+                Link::set_collision(link::CollisionType::Default);
+            }
+        }
+    }
+
+}
+
+#[no_mangle]
+#[inline(never)]
+pub extern "C" fn set_controller() {
+    if Link::stage() == stage::forsaken_fortress::FF1_INTERIOR
+    {
+        controller::mask_all_buttons(!controller::A);
     }
 }
 
@@ -50,4 +96,5 @@ pub extern "C" fn game_loop() {
 pub extern "C" fn start() {
     game_loop();
     init();
+    set_controller();
 }
--- src/src/patch.asm
+++ src/src/patch.asm
@@ -4,5 +4,9 @@ nop ; Get rid of ClearArena
 0x800063ec:
 bl init
 
-0x8000645C:
+0x80006458:
 bl game_loop
+bl 0x80022e74 ; fapGm_Execute__Fv
+
+0x80313b10:
+b set_controller
