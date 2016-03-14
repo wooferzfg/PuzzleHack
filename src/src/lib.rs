d767231b4ca449d25719bf895d4560f82f696c77
Sergey Papushin <spapushin@gmail.com>
2016-03-14 00:12:44 -0500

    Make save and death warps take you to empty room

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -16,6 +16,8 @@ use libtww::game::flag;
 use libtww::warping::Entrance;
 use libtww::warping::Warp;
 use libtww::warping::stage;
+use libtww::warping::warp;
+use libtww::warping::fadeout::FadeOut;
 
 #[no_mangle]
 #[inline(never)]
@@ -35,17 +37,23 @@ pub extern "C" fn game_loop() {
     let mut lines = &mut console.lines;
 
     let exit = Warp::last_exit();
-    
-    if exit.entrance.stage_name() == stage::sea::SEA && Link::stage() == stage::other::NAME_SELECT && !flag::HAS_SEEN_INTRO.is_active()
+
+    if exit.entrance.stage_name() == stage::sea::SEA && Entrance::last_entrance().stage_name() == stage::other::NAME_SELECT && !flag::HAS_SEEN_INTRO.is_active()
     {
         system::memory::write(0x803B81BC, 0x01000000); //hero's sword
         let warp = Warp::new(stage::earth_temple::TEMPLE, 1, 1, exit.layer_override, exit.fadeout, exit.enabled);
         warp.execute();
     }
-    if Link::stage() == stage::earth_temple::TEMPLE && exit.entrance.room == Entrance::last_entrance().room
+    else if exit.entrance.stage_name() == stage::sea::SEA && exit.entrance.room == 44 //game over or save warp
     {
+        let _ = write!(lines[0].begin(), "you tried... :^)");
+        let warp = Warp::new(stage::dev::LARGE_EMPTY_ROOM, 0, 0, exit.layer_override, exit.fadeout, true);
+        warp.execute();
+    }
+    else if Entrance::last_entrance().stage_name() == stage::earth_temple::TEMPLE && exit.entrance.room == Entrance::last_entrance().room
+    {
+        flag::HAS_SEEN_INTRO.activate();
         let _ = write!(lines[0].begin(), "Puzzle 1: Backwards is Forwards");
-
         if Link::room() == 0 && exit.entrance.room == Entrance::last_entrance().room
         {
             system::memory::write(0x803B81BC, 0x00000000); //hero's sword off
@@ -58,10 +66,8 @@ pub extern "C" fn game_loop() {
             warp.execute();
         }
     }
-
-    if Link::stage() == stage::forsaken_fortress::FF1_INTERIOR
+    else if Entrance::last_entrance().stage_name() == stage::forsaken_fortress::FF1_INTERIOR
     {
-
         if !flag::GRABBED_FIRST_ROPE_IN_FF1.is_active()
         {
             let _ = write!(lines[0].begin(), "Puzzle 2: On the Ropes Without That Button");
@@ -80,13 +86,16 @@ pub extern "C" fn game_loop() {
             }
         }
     }
-
+    else 
+    {
+        lines[0].clear();
+    }
 }
 
 #[no_mangle]
 #[inline(never)]
 pub extern "C" fn set_controller() {
-    if Link::stage() == stage::forsaken_fortress::FF1_INTERIOR
+    if Entrance::last_entrance().stage_name() == stage::forsaken_fortress::FF1_INTERIOR
     {
         controller::mask_all_buttons(!controller::A);
     }
