c1c1aa251b9fc5ab21986f383de6419b60f21bb7
Sergey Papushin <spapushin@gmail.com>
2016-03-20 15:12:55 -0500

    Add DRI equip based puzzle

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -8,6 +8,7 @@ use libtww::prelude::*;
 use libtww::{system, link, Link};
 use libtww::link::inventory::Inventory;
 use libtww::link::item;
+use libtww::link::equips::Equips;
 use libtww::link::quest_items::Sword;
 use libtww::game::{Console, controller, flag};
 use libtww::warping::{Entrance, Warp, stage};
@@ -22,6 +23,7 @@ pub extern "C" fn init() {
 }
 
 pub static mut hasSword : bool = true;
+pub static mut adjusted_index : u8 = 0;
 
 #[no_mangle]
 #[inline(never)]
@@ -29,6 +31,8 @@ pub extern "C" fn game_loop() {
     let link = Link::get();
     let console = Console::get();
     let inventory = Inventory::get();
+    let equips = Equips::get();
+
     let mut lines = &mut console.lines;
 
     let exit = Warp::last_exit();
@@ -107,9 +111,83 @@ pub extern "C" fn game_loop() {
         inventory.bombs_slot = item::BOMBS;
         inventory.tingle_tuner_slot = item::TINGLE_TUNER;
     }
+    else if Entrance::last_entrance().stage_name() == stage::dragon_roost_island::POND
+    {
+        let _ = write!(lines[0].begin(), "- X Controls The Items -");
+        
+        link.magic = 16;
+        link.max_magic = 16;
+        inventory.bomb_count = 99;
+
+        inventory.telescope_slot = item::TELESCOPE;
+        inventory.sail_slot = item::SAIL;
+        inventory.wind_waker_slot = item::WIND_WAKER;
+        inventory.grappling_hook_slot = item::GRAPPLING_HOOK;
+        inventory.tingle_tuner_slot = item::TINGLE_TUNER;
+        inventory.picto_box_slot = item::PICTO_BOX;
+        inventory.iron_boots_slot = item::IRON_BOOTS;
+        inventory.magic_armor_slot = item::MAGIC_ARMOR;
+
+        let new_adjusted_index = adjust_right_index(adjust_left_index(equips.x_index));
+
+        if new_adjusted_index != unsafe {adjusted_index}
+        {
+            Inventory::set_by_slot_id(unsafe{adjusted_index} as usize, item::EMPTY);
+            Inventory::set_by_slot_id(new_adjusted_index as usize, item_id_for_slot(new_adjusted_index));
+            unsafe{adjusted_index = new_adjusted_index;}
+        }
+    }
     else 
     {
         lines[0].clear();
+    }
+}
+
+//adjust the inventory index for left side of the inventory so that it ranges from 1 to 8
+pub fn adjust_left_index(index: u8) -> u8 {
+    let mut adjusted = 0;
+    if index != 255
+    {
+        if index < 4
+        {
+            adjusted = index + 1;
+        }
+        else if index >= 7 && index < 11
+        {
+            adjusted = index - 2;
+        }
+    }
+    adjusted
+}
+
+//adjust the 0 to 8 index so it corresponds to an index on the right side of the inventory
+pub fn adjust_right_index(index: u8) -> u8 {
+    if index < 3
+    {
+        return index + 4;
+    }
+    else if index < 6
+    {
+        return index + 8;
+    }
+    else 
+    {
+        return index + 12;
+    }
+}
+
+pub fn item_id_for_slot(index: u8) -> u8 {
+    match index {
+        4 => item::SPOILS_BAG,
+        5 => item::BOOMERANG,
+        6 => item::DEKU_LEAF,
+        11 => item::BAIT_BAG,
+        12 => item::BOW,
+        13 => item::BOMBS,
+        18 => item::DELIVERY_BAG,
+        19 => item::HOOKSHOT,
+        20 => item::SKULL_HAMMER,
+        _ => item::EMPTY,
     }
 }
 
@@ -143,7 +221,7 @@ pub extern "C" fn set_control_stuff() {
         let warp = Warp::new(stage::forbidden_woods::BOSS, 0, 0, exit.layer_override, exit.fadeout, true);
         warp.execute();
     }
-    else if exit.entrance.stage_name() == stage::forest_haven::FOREST_HAVEN || exit.entrance.stage_name() == stage::forsaken_fortress::FF1_INTERIOR
+    else if exit.entrance.stage_name() == stage::forest_haven::FOREST_HAVEN
     {
         inventory.bomb_count = 45;
         inventory.bomb_capacity = 99;
@@ -154,8 +232,8 @@ pub extern "C" fn set_control_stuff() {
     {
         if inventory.bomb_count == 42
         {
-            //next puzzle... somewhere
-            let warp = Warp::new(stage::dev::BASIC_ISLAND, 0, 0, exit.layer_override, exit.fadeout, true);
+            inventory.bombs_slot = item::EMPTY;
+            let warp = Warp::new(stage::dragon_roost_island::POND, 1, 0, exit.layer_override, exit.fadeout, true);
             warp.execute();
         }
         else 
@@ -164,6 +242,11 @@ pub extern "C" fn set_control_stuff() {
             let warp = Warp::new(stage::outset::UNDER_LINKS_HOUSE, 0, 0, exit.layer_override, exit.fadeout, true);
             warp.execute();
         }
+    }
+    else if exit.entrance.stage_name() == stage::dragon_roost_island::POSTAL_SERVICE
+    {
+        let warp = Warp::new(stage::dragon_roost_island::POND, 1, 0, exit.layer_override, exit.fadeout, true);
+        warp.execute();
     }
 }
 
